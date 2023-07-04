0522d4267d8c24a6293a434ce1223f3f622981a4
simar <2005simmy@gmail.com>
2023-07-05 00:23:39 +1200

    data binding demo

--- app/src/main/java/com/example/databindingdemo/MainActivity.kt
+++ app/src/main/java/com/example/databindingdemo/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.databindingdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.VISIBLE
+import androidx.databinding.DataBindingUtil
+import com.example.databindingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.button.setOnClickListener{
+            onButtonClicked()
+        }
+    }
+
+    private fun onButtonClicked() =
+        if (binding.progressCircular.visibility == View.GONE){
+        binding.progressCircular.visibility = VISIBLE
+        binding.button.text = "STOP"
+    }
+    else{
+        binding.progressCircular.visibility = View.GONE
+        binding.button.text = "START"
     }
 }
